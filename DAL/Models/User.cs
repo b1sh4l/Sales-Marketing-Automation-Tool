407f78ebbbfe9c56d1f6014e08a9074792d4e925
b1sh4l <sudarshansahabishal@gmail.com>
2023-12-30 16:30:17 +0600

    Add project files.

---
+++ DAL/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    internal class User
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string Uname { get; set; }
+
+
+        [Required, StringLength(20)]
+        public string Password { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(11)]
+        public string MobileNo { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(20)]
+        public string Type { get; set; }
+    }
+}
---
+++ Sales-Marketing-Automation-Tool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sales_Marketing_Automation_Tool.WebApiApplication" Language="C#" %>
